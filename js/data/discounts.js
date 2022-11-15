3d554627ca48adeff0c481dd6518fd1062929ed2
Penny Redman <penny@nuvola.co.uk>
2022-11-15 17:21:09 +0000

    Woolshop v1
    Initial changes from shop to woolshop. Adding additional pages, navigation, links and styling. JS not changed at this point

--- js/data/discounts.js
+++ js/data/discounts.js
@@ -4,13 +4,13 @@ see the codes. In a real world example we would add in an API to verify the disc
 window.discounts = [
     {code:"GIMME2",	amount:2},
     {code:"GIMME5",	amount:5},
-    {code:"GIMME10",	amount:10},
+    {code:"GIMME10", amount:10},
 ];
 
 // Returns the discount where the code matches the supplied parameter
 function getDiscountByCode(code) {
     let discounts = window.discounts.filter(discount => discount.code === code);
 
-    // if the discount code doesn't exist, this will return null
+// if the discount code doesn't exist, this will return null
     return discounts[0] || null;
 }
